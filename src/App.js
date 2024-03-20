1c101e648712ee9d1f828bc49e1547e8b73c8436
Raphael Prudencio Silva <raphaelprudencio@hotmail.com>
2024-03-20 13:45:19 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Em desenvolvimento.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cgsoft.com.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CGSOFT
         </a>
       </header>
     </div>
